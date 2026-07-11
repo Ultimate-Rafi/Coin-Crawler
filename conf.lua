88062465ea28801190f96aad8250c1adfbd2ae25
Ultimate-Rafi <ultimateranger999999999@gmail.com>
2026-07-11 12:48:35 +0600

    Added the game files

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.resizable = false
+    t.window.orientation = "landscape" -- Forces the app to open horizontally
+    t.identity = "my_custom_alpha_game" -- Sets a unique sandbox folder for saving game files
+end
